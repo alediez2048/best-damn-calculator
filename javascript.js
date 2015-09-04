868a0cf57f060a887d47c281cafcf1402c12e548
Erich Abbott <abbotterich@gmail.com>
2015-09-04 10:43:00 -0500

    10am js

---
+++ javascript.js
@@ -0,0 +1,27 @@
+function calculator (argument) {
+
+	if (addition) {
+
+		add = function addition();
+
+	}else if (subtraction) {
+
+		subtract  = function subtraction();
+
+	}else if (multiplication) {
+
+		times = function multiplication();
+
+	}else if (division) {
+
+		divide = function division();
+
+	}else {
+
+		function null();
+	}
+
+
+
+		var 
+}
